77057780b370165631ef5e4cdaa7190fdeffdad3
SamUniverse123 <62712581+SamUniverse123@users.noreply.github.com>
2024-04-30 09:28:39 +0800

    commit website 5(Calnot)

---
+++ components/shared/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+
+function Header() {
+  return (
+    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-4 dark:bg-neutral-800">
+  <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+    <a className="flex-none text-xl font-semibold dark:text-white" href="#">Brand</a>
+    <div className="flex flex-row items-center gap-5 mt-5  mr-10 sm:justify-end sm:mt-0 sm:ps-5 ">
+      <NavBar />
+    </div>
+  </nav>
+</header>
+  )
+}
+
+export default Header
